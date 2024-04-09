a5abf6042cb12528a1d3d457abaa6dd239bd325c
BPaul04 <165049824+BPaul04@users.noreply.github.com>
2024-04-09 17:23:27 +0200

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -13,5 +13,32 @@ void initialisation(struct poisson P, double x_max, double y_max){
     srand(time(NULL));
     P.x = (float)rand()/(float)RAND_MAX * x_max;
     P.y = (float)rand()/(float)RAND_MAX * y_max;
-    P.dir = (float)rand()/(float)RAND_MAX * 2 * M_PI;
+    P.dir = (float)rand()/(float)RAND_MAX * 2 * 360;
 }
+
+//Distance entre 2 poissons
+float distance(struct poisson A, struct poisson B){
+    return sqrt( (B.x-A.x)*(B.x-A.x) + (B.y-A.y)*(B.y-A.y) );
+}
+
+//Test de l'angle mort
+int dans_angle_mort(struct poisson A, struct poisson B){
+// On calcule les angles
+    if(){ //Le poisson est dans l'angle mort
+        return 1; 
+    }else{
+        return 0;
+    }
+}
+
+void traitement(int* indices_za, int* indices_zr, int* indices_zo, int indP, double* dir_temp, int N){
+    float d_r = 0;
+    float d_o = 0;
+    float d_a = 0;
+
+    for(int i=0; i<N, i++){
+        
+    }
+
+}
+
