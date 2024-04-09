ebf700d7f8b7b197d82c1c50b6f706984f64570c
BPaul04 <165049824+BPaul04@users.noreply.github.com>
2024-04-09 17:24:10 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -1,4 +1,13 @@
 
-
+struct poisson{
+    double x; //Position abscisses
+    double y; //Position ordonnées
+    double dir; //Direction de la vitesse
+};
 
 void initialisation(struct poisson P, double x_max, double y_max);
+
+float distance(struct poisson A, struct poisson B);
+
+int dans_angle_mort(struct poisson A, struct poisson B);
+
