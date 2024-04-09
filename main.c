e866b418a834930b9dacd698edae25fad1ca7e1a
BPaul04 <paul.bats@ensta-paris.fr>
2024-04-09 15:21:53 +0200

    fichier fonctions et modification du main

---
+++ main.c
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+struct poisson{
+    double pos; //Position
+    double dir; //Direction de la vitesse
+}
+
+
+
+
+int main(){
+
+int N; //Nombre de poissons
+double vit; //Norme de la vitesse des poissons
+double percep; //Champ de perception
+double attract; //Rayon de la zone d'attraction
+double repuls; //Rayon de la zone de répulsion
+double orient; //Rayon de la zone d'orientation
+}
