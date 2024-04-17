94266dcd22178f70b46dbd5a7d3d4d6441b06cdd
BPaul04 <paul.bats@ensta-paris.fr>
2024-04-17 14:45:20 +0200

    évolution des fonctions

--- fonctions.c
+++ fonctions.c
@@ -13,7 +13,7 @@ void initialisation(struct poisson P, double x_max, double y_max){
     srand(time(NULL));
     P.x = (float)rand()/(float)RAND_MAX * x_max;
     P.y = (float)rand()/(float)RAND_MAX * y_max;
-    P.dir = (float)rand()/(float)RAND_MAX * 2 * 360;
+    P.dir = (float)rand()/(float)RAND_MAX * 360;
 }
 
 //Distance entre 2 poissons
@@ -31,14 +31,52 @@ int dans_angle_mort(struct poisson A, struct poisson B){
     }
 }
 
-void traitement(int* indices_za, int* indices_zr, int* indices_zo, int indP, double* dir_temp, int N){
-    float d_r = 0;
-    float d_o = 0;
-    float d_a = 0;
+/* --------------------------------
+Fonctions nécessaires au traitement
+*/
+double repulsion(int* indices_zr, int N, struct poisson* banc){
+    int compt = 0;
+            for(int j=0; j<N; j++){
+                tmp = tmp + indices_zr[j]*(banc[j].dir+180); // +180 ~ *-1
+                compt++;
+            }
+            return(tmp/compt);
+}
+
+double attraction(int* indices_za, int N, struct poisson* banc){
+    int compt = 0;
+            for(int j=0; j<N; j++){
+                tmp = tmp + indices_za[j]*(banc[j].dir);
+                compt++;
+            }
+            return(tmp/compt);
+}
+
+double orientation(int* indices_zo, int N, struct poisson* banc){
+    
+}
+
+
+void traitement(int* indices_za, int* indices_zr, int* indices_zo, double* dir_temp, int N, struct poisson* banc, int indP, float s){
+    /* indices_za, indices_zr et indices_zo indiquent la position des poissons par rapport aux zones
+    du poisson en question.
+    
+    J'ai ajouté le banc de poisson qu'on a oublié.*/
+    double d_r = 0;
+    double d_o = 0;
+    double d_a = 0;
+
+    double tmp = 0; // Variable temporaire
+
+    // Pour éviter de créer des boucles trop similaires, je crée trois autres fonctions : repulsion(), attraction() et orientation().
 
     for(int i=0; i<N; i++){
-        
+        if(indices_zr[i]){// Si au moins un poisson est dans la zone de répulsion, on ne fait pas les autres boucles.
+            dir_temp[indP] = repulsion(indices_zr, N, banc);
+            return(0);
+        }
     }
+    // Si on est sorti de cette boucle, c'est qu'aucun poisson n'est dans la zone de répulsion. 
 
 }
 
--- fonctions.h
+++ fonctions.h
@@ -11,4 +11,10 @@ float distance(struct poisson A, struct poisson B);
 
 int dans_angle_mort(struct poisson A, struct poisson B);
 
-void traitement(int* indices_za, int* indices_zr, int* indices_zo, int indP, double* dir_temp, int N)
+double repulsion(int* indices_zr, int N, struct poisson* banc);
+
+double attraction(int* indices_za, int N, struct poisson* banc);
+
+double orientation(int* indices_zo, int N, struct poisson* banc);
+
+void traitement(int* indices_za, int* indices_zr, int* indices_zo, double* dir_temp, int N, struct poisson* banc, int indP, float s);
